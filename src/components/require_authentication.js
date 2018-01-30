fce3011790f4be087695785dbac922eb1ccb50c7
Casey Kelly <casey.kelly@colorado.edu>
2018-01-30 16:12:42 -0700

    signout user and redirect to homepage if not authenticated

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -4,17 +4,28 @@ import { connect } from "react-redux";
 export default function(ComposedComponent) {
   class Authentication extends Component {
     static contextTypes = {
-        router: React.PropTypes.object
+      router: React.PropTypes.object
+    };
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push("/");
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push("/");
+      }
     }
 
     render() {
-      console.log(this.context)
       return <ComposedComponent {...this.props} />;
     }
   }
 
   function mapStateToProps(state) {
-      return { authenticated: state.authenticated };
+    return { authenticated: state.authenticated };
   }
 
   return connect(mapStateToProps)(Authentication);
